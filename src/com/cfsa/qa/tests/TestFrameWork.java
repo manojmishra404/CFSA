e781b45175fef3991580e1e8dae2c710bc382b08
hari-shankar-bng <Hari_shankar-BNG@external.mckinsey.com>
2016-05-29 15:57:27 +0530

    created page and test

---
+++ src/com/cfsa/qa/tests/TestFrameWork.java
@@ -0,0 +1,45 @@
+package com.cfsa.qa.tests;
+
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+public class TestFrameWork implements ITestListener {
+    public void onTestStart(ITestResult iTestResult) {
+
+    }
+
+    public void onTestSuccess(ITestResult iTestResult) {
+
+    }
+
+    public void onTestFailure(ITestResult iTestResult) {
+
+    }
+
+    public void onTestSkipped(ITestResult iTestResult) {
+
+    }
+
+    public void onTestFailedButWithinSuccessPercentage(ITestResult iTestResult) {
+
+    }
+
+    public void onStart(ITestContext iTestContext) {
+
+    }
+
+    public void onFinish(ITestContext iTestContext) {
+
+    }
+
+
+
+
+
+
+
+
+
+
+}
