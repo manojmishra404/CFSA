9d4f34eea375015a35cf4944a841b462503120c7
mmishra <mmishra@IM-RT-LP-079.INNOMINDSBANG.COM>
2016-06-13 11:26:49 +0530

    Commit for screenshot, xslt report

--- src/com/cfsa/qa/pages/DashboardPage.java
+++ src/com/cfsa/qa/pages/DashboardPage.java
@@ -6,6 +6,7 @@ package com.cfsa.qa.pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.CacheLookup;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
 
@@ -20,7 +21,8 @@ public class DashboardPage extends Page {
 		this.driver = driver;
 		// TODO Auto-generated constructor stub
 	}
-
+	
+	@CacheLookup
 	@FindBy(how = How.XPATH, using = "//span[@class='pull-right new-client-action']/i")
 	WebElement NewClientBtn;
 	
